d57f5578b4a2893a08e96760d7bcaa26f1d424b7
Andreas Karz <Andreas.Karz@swisslife.ch>
2022-01-15 17:48:47 +0100

    Basic Init

---
+++ src/src/test/java/org/example/StepDefinitions.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import org.junit.jupiter.api.Assertions.*;
+
+
+public class StepDefinitions {
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println ("Funky");
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
