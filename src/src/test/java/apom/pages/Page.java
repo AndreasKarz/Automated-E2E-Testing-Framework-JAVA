b3bafd37b6f3c308308b4c26e90938d80afc36c7
Andreas Karz <Andreas.Karz@swisslife.ch>
2022-01-17 12:41:27 +0100

    First steps

---
+++ src/src/test/java/apom/pages/Page.java
@@ -0,0 +1,11 @@
+package apom.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+    public String title;
+
+    public BasePage(WebDriver driver){
+
+    }
+}
---
+++ src/src/test/java/apom/pages/StartPage.java
@@ -0,0 +1,2 @@
+package apom.pages;public class StartPage {
+}
